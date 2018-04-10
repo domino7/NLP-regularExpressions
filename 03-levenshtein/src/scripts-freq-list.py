d02ee40e1405c9f27eace160fd7b38ee3c69a68c
domino7 <dominik.goralczyk@gmail.com>
2018-04-10 10:30:07 +0200

    lab3 further solution

--- 03-levenshtein/src/scripts-freq-list.py
+++ 03-levenshtein/src/scripts-freq-list.py
@@ -3,8 +3,6 @@ import io
 import json
 from glob import glob
 import re
-from elasticsearch import Elasticsearch
-import pandas as pd
 
 
 def getContent(jsons_dir, output_dir):
@@ -74,75 +72,4 @@ results_sample_dir = "../data/results2015_trial.csv"
 
 results_full_dir = "../data/results2015.csv"
 # getFrequencyList(output_full, results_full_dir)
-
-
-# get all words that not exist in dictionary
-
-es = Elasticsearch()
-
-def prepareJson(text):
-    json_to_return = {
-      "analyzer": "morfologik",
-      "text": text
-    }
-    return json_to_return
-
-
-def word_found(word):
-    single_json = prepareJson(word)
-    es_response = es.index(index="test", doc_type="_analyze", body=single_json)
-    basic_form = es_response['tokens'][0]['token']
-    if basic_form != word:
-        return True
-    # extra check if basic form is the same
-    morfologic_dir = "../polimorfologik-2/polimorfologik-2.1.txt"
-    with open(morfologic_dir, 'r') as f:
-        data = list(f.read().splitlines())
-        for pos in data:
-            if pos[0] < word[0]:
-                pass
-            elif pos[0] == word[0]:
-                if pos.startswith(basic_form+";"):
-                    custom_form = pos.split(";")[1]
-                    if word == custom_form:
-                        return True
-            else:
-                return False
-    return False
-
-
-def find_inproper_words(csv_file, inproper_words_file):
-    results = pd.read_csv(csv_file, header=None, names=['word', 'counter'])
-    words = results['word']
-    i = 0
-    with io.open(inproper_words_file, 'w') as results:
-        for single_word in words:
-            i = i+1
-            if i%50 == 0:
-                print("already done words: ", i)
-            if not word_found(single_word):
-                results.write(single_word)
-                results.write("\n")
-
-
-
-def get_words_not_normalised(csv_file, inproper_words_file):
-    results = pd.read_csv(csv_file, header=None, names=['word', 'counter'])
-    words = results['word']
-    i = 0
-    with io.open(inproper_words_file, 'w') as results:
-        for single_word in words:
-            i = i + 1
-            single_json = prepareJson(single_word)
-            es_response = es.index(index="test", doc_type="_analyze", body=single_json)
-            basic_form = es_response['tokens'][0]['token']
-            if basic_form == single_word:
-                results.write(single_word)
-                results.write("\n")
-
-csv_file = "../data/results2015_trial.csv"
-inproper_words_file = "../data/inproper_words_trial.txt"
-get_words_not_normalised(csv_file, inproper_words_file)
-
-print("\nDONE")
 
